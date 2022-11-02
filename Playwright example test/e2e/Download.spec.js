a1f9c067150957f6b20bf47c98da6c667b164f7b
piotrk39 <piotrk39@gmail.com>
2022-11-02 14:01:26 +0200

    Add download test on external website

--- Playwright example test/e2e/Download.spec.js
+++ Playwright example test/e2e/Download.spec.js
@@ -12,15 +12,6 @@ test.describe("Download test", () => {
         const download = new sampleDownload(page);
         
         await download.redirectToCvDownload();
-        const [ downl ] = await Promise.all([
-            // Start waiting for the download
-            page.waitForEvent('download'),
-            // page.on('download', download => download.path().then(console.log)),
-            // Perform the action that initiates download
-            download.download.click(),
-            download.downloadDirect.click(),
-          ]);
-          // Wait for the download process to complete
-          console.log(await downl.path());
+        await download.downloadFile(download.download, page);
     });
 });
--- Playwright example test/sampleData/downloadData.js
+++ Playwright example test/sampleData/downloadData.js
@@ -7,12 +7,20 @@ export default class sampleDownload {
     constructor(page) {
       this.resume = page.getByRole('link', { name: 'Resume' });
       this.download = page.getByRole('button', { name: 'Download CV' }).first();
-      this.downloadDirect = page.locator('//*[@id="icon"]/iron-icon')
     }
 
     async redirectToCvDownload() {
       await this.resume.click();
       await expect(this.download).toHaveText('Download CV');
-    //   await downloadFile();
+    }
+
+    async downloadFile(locator, page)  {
+      const [ download ] = await Promise.all([
+          page.waitForEvent('download'),
+          locator.click({ modifiers: ["Alt",] })
+        ]);
+  
+        // const path = await download.path();
+        // return path;
     }
 }
